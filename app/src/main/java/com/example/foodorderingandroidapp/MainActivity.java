ca943ca6b2020dd26290441f4cf0b85f8e0b40d1
andrei <git1@email.com>
2021-11-07 07:50:10 +0100

    Fethched data from server
    * Retrieved data from server using  Retrofit Get call from API Interface from Json object

--- app/src/main/java/com/example/foodorderingandroidapp/MainActivity.java
+++ app/src/main/java/com/example/foodorderingandroidapp/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.foodorderingandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.foodorderingandroidapp.model.FoodData;
+import com.example.foodorderingandroidapp.retrofit.ApiInterface;
+import com.example.foodorderingandroidapp.retrofit.RetrofitClient;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    ApiInterface apiInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        apiInterface = RetrofitClient.getRetrofitInstance().create(ApiInterface.class);
+
+        Call<List<FoodData>> call = apiInterface.getAllData();
+        call.enqueue(new Callback<List<FoodData>>() {
+            @Override
+            public void onResponse(Call<List<FoodData>> call, Response<List<FoodData>> response) {
+
+                List<FoodData> foodDataList = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<List<FoodData>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Server is not responding.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/foodorderingandroidapp/retrofit/ApiInterface.java
+++ app/src/main/java/com/example/foodorderingandroidapp/retrofit/ApiInterface.java
@@ -1,9 +1,15 @@
 package com.example.foodorderingandroidapp.retrofit;
 
+import com.example.foodorderingandroidapp.model.FoodData;
+
+import java.util.List;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiInterface {
 
     @GET("fooddata.json")
+    Call<List<FoodData>> getAllData();
 
 }
